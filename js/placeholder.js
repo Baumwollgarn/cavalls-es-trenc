2810600e0abfe76315d2377ae37f9102e40c61a3
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-04-20 12:07:54 +0200

    Buy API REST insert
    Added script to fetch the API REST json (for now a fake one).
    Added the buy part itself, styled it and made some changes to it.

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,3 +1,35 @@
 function toggleMenu() {
     document.getElementById("navigation").classList.toggle("responsive");
 }
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        for (const dataItem of data) {
+            createOffer(dataItem);
+        }
+    })
+
+let card = document.querySelector('.offers');
+let cardContent = document.createElement('div');
+cardContent.className = 'card-content';
+
+function createOffer(data) {
+    cardContent.innerHTML += 
+        
+            `<div class="offer-card">
+            <img src="https://picsum.photos/seed/${data.id + data.name + 10}/500/500" alt="photo">
+            <h1>${data.name}</h1>
+            <p>${data.email}</p>
+            <p>${data.phone}</p>
+            <p>${data.website}</p>
+            <h2 class="price">${generatePrice()} € </h2>
+            </div>
+            `;
+    
+    card.appendChild(cardContent);
+}
+
+let generatePrice = () => {
+    return Math.floor(Math.random() * (1000 - 10) + 10);
+}
